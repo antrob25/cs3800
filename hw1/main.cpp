d6f282669dd82f871ab402dd2429ba444120d355
Anthony Robles <argk4@umsystem.edu>
2020-09-14 22:14:23 -0500

    Add files via upload

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -40,13 +40,88 @@ int main(int argc, char* argv[])
 
     // Read in from the file
     initProcessSetFromFile(file, processList);
-
-    while()
+    unsigned long processes = processList.size();
+    unsigned long* current_index = new unsigned long [processes];
+    unsigned long** event_index = new unsigned long* [processes];
+    for ( unsigned long i = 0; i < processes; i++ )
+    {
+        event_index[i] = new unsigned long [processList.at(i).ioEvents.size()];
+        current_index[i] = 0;
+        event_index[i][current_index[i]] = 0;
+    }         
+    while( allProcessesComplete(processList) == false )
     {
         // Set state of all processes that should be ready to ready
         // hint: arrival time and processes becoming unblocked
-        
 
+        for ( unsigned long i = 0; i < processes; i++ )
+        {
+            if ( processorAvailable == true )
+            {
+                for ( unsigned long j = 0; j < processes; j++ )
+                {
+                    if ( processList.at(j).state == ready && processorAvailable == true)
+                    {
+                        processorAvailable = false;
+                        processList.at(j).state = processing;
+                    }
+                } 
+            }    
+
+            if ( processList.at(i).state == notArrived )
+            {
+                if ( processList.at(i).arrivalTime == time )
+                {
+                    processList.at(i).state = ready;
+                    if ( processList.at(i).ioEvents.size() != 0 )
+                        processList.at(i).currentEvent = processList.at(i).ioEvents.front();
+
+                    if ( processList.at(i).reqProcessorTime == 0 )
+                    {
+                        processList.at(i).state = done;
+                        processList.at(i).processorTime = 0;
+                        processList.at(i).doneTime = time;
+                    }
+                }
+            }
+
+            if ( processList.at(i).state == blocked )
+            {
+                if ( static_cast<long>(event_index[i][current_index[i]]) == processList.at(i).currentEvent.duration )
+                {
+                    processList.at(i).state = ready;
+
+                    if ( current_index[i] < processList.at(i).ioEvents.size() )
+                    {
+                        current_index[i]++;
+                        processList.at(i).currentEvent = processList.at(i).ioEvents.at(current_index[i]);
+                    }
+                }
+
+                else
+                    event_index[i][current_index[i]]++;        
+            }
+
+            if ( processList.at(i).state == processing )
+            {
+                if ( processList.at(i).processorTime == processList.at(i).reqProcessorTime )
+                {
+                    processList.at(i).state = done;
+                    processList.at(i).doneTime = time;
+                    processorAvailable = true;
+                }
+
+                else if ( processList.at(i).ioEvents.size() != 0 && processList.at(i).currentEvent.time == processList.at(i).processorTime )
+                {
+                    processList.at(i).state = blocked;
+                    processorAvailable = true;
+                }
+
+                else 
+                    processList.at(i).processorTime++;
+            }     
+        }
+        printProcessSet(processList);
         // Select the next process that should be given to the processor
 
         // Increment the selected process's processorTime
@@ -63,6 +138,16 @@ int main(int argc, char* argv[])
         printProcessStates(processList); // change processList to another vector of processes if desired
         this_thread::sleep_for(chrono::milliseconds(sleepDuration)); // Do not touch
     }
+
+    // freeing the memory used in the program 
+    for ( unsigned long i = 0; i < processes; i++ )
+    {
+        delete[] event_index[i];
+    }
+    delete[] event_index;
+	event_index = NULL;
+    delete[] current_index;
+    current_index = NULL;
 
     return 0;
 }
